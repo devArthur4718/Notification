c3b366643595a7f6a1e459cec3da6f4ff2563919
Arthur Gomes <devarthur4718@gmail.com>
2020-05-01 22:32:54 -0300

    first commit

---
+++ app/src/main/java/com/devarthur4718/services/AlertDetails.kt
@@ -0,0 +1,12 @@
+package com.devarthur4718.services
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AlertDetails : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_alert_details)
+    }
+}
---
+++ app/src/main/java/com/devarthur4718/services/MainActivity.kt
@@ -0,0 +1,70 @@
+package com.devarthur4718.services
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    @SuppressLint("ResourceType")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        btnNotification.setOnClickListener {
+            it.context.showNotification(
+                title = getString(R.string.title_not_1),
+                comment = getString(R.string.services_notification),
+                iconDrawable = R.drawable.ic_android_black_24dp
+            )
+        }
+
+        btnNotificationTap.setOnClickListener {
+            it.context.showNotificationTap(
+                R.drawable.ic_android_black_24dp,
+                "Notification Tap",
+                 "Notification Content",
+                "11",
+                AlertDetails::class.java,
+                false
+
+            )
+        }
+
+        btnShowToast.setOnClickListener {
+            it.context.showToast(R.string.app_name)
+        }
+
+        btnShowNowtificationAction.setOnClickListener {
+            it.context.showNotificationActionButton(
+                R.drawable.ic_android_black_24dp,
+                "Notification Action",
+                "Notification content",
+                "Show all messages",
+                "11",
+                AlertDetails::class.java
+            )
+        }
+
+        btnShowReplyButton.setOnClickListener {
+
+            it.context.showNotificationReply(
+                R.drawable.ic_message_black_24dp,
+                R.drawable.ic_screen_share_black_24dp,
+                            "Do you want to reply?",
+                            "Notifiation Content",
+                            "Reply 2",
+                                "12",
+                        AlertDetails::class.java
+            )
+        }
+
+
+    }
+
+
+
+
+}
